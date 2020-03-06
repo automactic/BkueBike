6aafcfd174128694667f3e813920bf333dfcd988
ChrisLi <chris.li@datarobot.com>
2020-03-06 14:01:47 -0500

    aiohttp

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import asyncio
 import logging
 
-from data_sources import Stations, Regions, Trips
+import aiohttp
+
 from database import Database
 from pipeline import TrainingData, Scoring, Actuals, DataImporter
 
@@ -20,10 +21,14 @@ async def import_and_update_database():
 
 
 async def score():
-    scoring = Scoring()
-    while True:
-        scoring.predict()
-        await asyncio.sleep(10)
+    async with aiohttp.ClientSession() as session:
+        scoring = Scoring(session)
+        while True:
+            try:
+                await scoring.predict()
+                await asyncio.sleep(10)
+            except Exception:
+                await asyncio.sleep(100)
 
 
 async def actual_submit():
@@ -34,19 +39,17 @@ async def actual_submit():
 
 
 if __name__ == '__main__':
-    export_training_data()
+    # initialization
+    Database.create_table()
+    Database.create_index()
 
-    # # initialization
-    # Database.create_table()
-    # Database.create_index()
-    #
-    # logging.basicConfig()
-    # logging.getLogger().setLevel(logging.DEBUG)
-    #
-    # # start run loop
-    # loop = asyncio.get_event_loop()
-    # loop.create_task(import_and_update_database())
-    # loop.create_task(score())
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+
+    # start run loop
+    loop = asyncio.get_event_loop()
+    loop.create_task(import_and_update_database())
+    loop.create_task(score())
     # loop.create_task(actual_submit())
-    # loop.run_forever()
-    # loop.close()
+    loop.run_forever()
+    loop.close()
