69a7c00c9d4674ba6881f1bddd98310d59a66c1b
ChrisLi <chris.li@datarobot.com>
2020-06-17 17:38:50 -0400

    DR env var

--- pipeline/base.py
+++ pipeline/base.py
@@ -1,6 +1,8 @@
-from contextlib import asynccontextmanager
-from aiohttp import ClientSession
+import os
 import typing
+from contextlib import asynccontextmanager
+
+from aiohttp import ClientSession
 
 
 class HTTPSessionMixin:
@@ -8,6 +10,29 @@ class HTTPSessionMixin:
         super().__init__(*args, **kwargs)
 
     @asynccontextmanager
-    async def create_session(self) -> typing.AsyncContextManager[ClientSession]:
-        async with ClientSession() as session:
+    async def create_session(self, headers: dict = None) -> typing.AsyncContextManager[ClientSession]:
+        headers = headers if headers else {}
+        async with ClientSession(headers=headers) as session:
             yield session
+
+
+class DataRobotEnvMixin:
+    api_endpoint = os.getenv('DATAROBOT_ENDPOINT')
+    api_token = os.getenv('DATAROBOT_API_TOKEN')
+    pred_server = os.getenv('DATAROBOT_PRED_ENDPOINT')
+    pred_server_key = os.getenv('DATAROBOT_KEY')
+    username = os.getenv('DATAROBOT_USERNAME')
+    deployment_id = os.getenv('DEPLOYMENT_ID')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        fields = [
+            self.api_endpoint,
+            self.api_token,
+            self.pred_server,
+            self.pred_server_key,
+            self.username,
+            self.deployment_id
+        ]
+        if not all(fields):
+            raise ValueError('Missing DataRobot environment variables', *fields)
